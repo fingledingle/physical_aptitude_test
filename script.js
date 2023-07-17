8ba7b09e2000bf906e463ced6735ccbc81a1e010
marky <marky02381@outlook.jp>
2023-07-18 07:11:55 +0800

    Tarefa 2 - Estruturas condicionais e operadores

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Variáveis para aplicar os resultados do teste. Você deverá categoriza-las como let/const, mas não deve alterar os valores.
 const gender = process.argv[2]; // Genero 
 const height = process.argv[3]; // Altura
-let barReps = process.argv[4]; // Repetições com barra
-let barSeconds = process.argv[5]; // Tempo das repetições com barra 
-let abs = process.argv[6]; // Abdominais
-let runDistance = process.argv[7]; // Distância da corrida
-let runTime = process.argv[8]; // Tempo da corrida
-let swimDistance = process.argv[9]; // Distância da natação
-let swimTime = process.argv[10]; // Tempo da natação
-let diveTime = process.argv[11]; // Tempo de mergulho
+const barReps = process.argv[4]; // Repetições com barra
+const barSeconds = process.argv[5]; // Tempo das repetições com barra 
+const abs = process.argv[6]; // Abdominais
+const runDistance = process.argv[7]; // Distância da corrida
+const runTime = process.argv[8]; // Tempo da corrida
+const swimDistance = process.argv[9]; // Distância da natação
+const swimTime = process.argv[10]; // Tempo da natação
+const diveTime = process.argv[11]; // Tempo de mergulho
 
-passed = false;
+let passed = false;
 
 /*
     Importante:
@@ -22,6 +22,40 @@ passed = false;
     ...
 */
 // **sua lógica a partir daqui**
+    if (gender === "male") {
+        if (height >= 1.70) {
+             if (barReps >= 6 || barSeconds >= 15) {
+                 if (abs >= 41) {
+                     if (runDistance >= 3000 && runTime >= 720 || runDistance >= 500 && runTime >= 1200){
+                         if (swimDistance >= 100 && swimTime >= 60 || diveTime == 30){
+                                passed = true; 
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+                                
+
+        
+    if (gender === "female") { 
+        if (height >= 1.60) {
+            if (barReps >= 5 || barSeconds >= 12) {
+                if (abs >= 41){
+                    if (runDistance >= 4000 && runTime >= 900 || runDistance >= 6000 && runTime >= 1320){
+                        if (swimDistance >= 100 && swimTime >= 60 || diveTime == 30){
+                            passed = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+            
+
 
 
 
